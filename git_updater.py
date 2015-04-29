b750c66b8528b9e945ec19fb7a5c3d1af2754259
Matthew Harris <mattbenharris@gmail.com>
2015-04-29 12:02:32 -0700

    MBH: cleanup file prems, adding repo checker

---
+++ git_updater.py
@@ -0,0 +1,22 @@
+#!/bin/python
+
+import os
+from git import Repo
+from remote_model.github import GithubModel
+from  local_settings import *
+
+
+for org in organizations:
+    current_org = "/orgs/" + org + "/repos"
+    current_directory = base_directoy + org
+    repos = GithubModel(current_org)
+
+    for repo in repos:
+        current_repo = os.path.join(current_directory, repo['name'])
+        if os.path.isdir(current_repo):
+            print current_repo
+            this_repo = Repo(current_repo)
+            this_repo.git.fetch('--all')
+        else:
+            #clone repo
+            print 'no'
