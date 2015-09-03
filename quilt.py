a38682ce4bc6e90145689608a2796ca3ae04f4c5
Matthew Harris <mattbenharris@gmail.com>
2015-09-03 10:34:29 -0700

    Reformatted output and refactored quilt.py for great quilting victory

--- quilt.py
+++ quilt.py
@@ -3,37 +3,38 @@ import random
 import argparse
 
 def pattern(v, h, n, t):
-    blocks = []
-    all_blocks = []
+    strlen = len(' %d ' % n)
+    pool = range(1,n+1) * t
+
+    # Hack to avoid 5's
+    pool = pool[:4] + pool[5:]
+    pool = pool[:n+3] + pool[n+4:]
+
+    random.shuffle(pool)
+    blocks = [[pool[row * h + col] for col in range(h)] for row in range(v)]
+
     d = "+"
-    for h in range(0, int(h)):
-        d = d + "---+"
-
-    for i in range (1,int(n)+1):
-        blocks.append(i)
-
-    for j in range (0, int(t)):
-        all_blocks += blocks
-
-    random.shuffle(all_blocks)
-    for k in range (0, 2):
-        all_blocks.remove(5)
-
+    # Scan each column for width
+    for _ in range(h):
+        d += "-" * strlen + "+"
     print d
-    for l in range(0,int(v)):
-        line = "| "
-        for m in range (0, int(h)):
-            current = all_blocks.pop(len(all_blocks)-1)
-            line = line + str(current) + " | "
+    for l in range(v):
+        line = "|"
+        for m in range(h):
+            current = blocks[l][m]
+            current = " %d " % current
+            while len(current) < strlen:
+                current += " "
+            line += str(current) + "|"
         print line
         print d
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="patterns for disappearing nine quilt pattern")
-    parser.add_argument("v", help="number of vertical quilt block")
-    parser.add_argument("h", help="number of horizontal quilt blocks")
-    parser.add_argument("n", help="number of quilt blocks patterns")
-    parser.add_argument("t", help="times to use each block number of horizontal quilt blocks required")
+    parser.add_argument("v", type=int, help="number of vertical quilt block")
+    parser.add_argument("h", type=int, help="number of horizontal quilt blocks")
+    parser.add_argument("n", type=int, help="number of quilt blocks patterns")
+    parser.add_argument("t", type=int, help="times to use each block number of horizontal quilt blocks required")
     args = parser.parse_args()
  
     pattern(args.v, args.h, args.n, args.t)
